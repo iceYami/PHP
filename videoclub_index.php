2da157ae0ddc0d5acb0ad7e2374e99d31d91b37c
iceYami <137309137+iceYami@users.noreply.github.com>
2024-10-20 09:34:43 +0200

    Create videoclub_index.php

---
+++ videoclub_index.php
@@ -0,0 +1,5 @@
+// Página de Inicio (index.php)
+<h1>Bienvenido al Videoclub</h1>
+<a href="registro.php">Registro</a>
+<a href="login.php">Iniciar Sesión</a>
+<a href="login_admin.php">Iniciar Sesión como Administrador</a>

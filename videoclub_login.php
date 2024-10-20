b3f70fea199c001cdf6a9c078872ee6cfe92495e
iceYami <137309137+iceYami@users.noreply.github.com>
2024-10-20 09:30:53 +0200

    Create videoclub_login.php

---
+++ videoclub_login.php
@@ -0,0 +1,8 @@
+// Login de Usuarios (login.php)
+
+<h2>Iniciar Sesión</h2>
+<form method="POST" action="autenticar_usuario.php">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="contrasena" placeholder="Contraseña" required>
+    <button type="submit">Entrar</button>
+</form>

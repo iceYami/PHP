3ed78dbc3fb98aa207b098c0ca15d74a3018ba82
iceYami <137309137+iceYami@users.noreply.github.com>
2024-10-20 09:33:21 +0200

    Create videoclub_login_admin.php

---
+++ videoclub_login_admin.php
@@ -0,0 +1,8 @@
+// Login del Administrador (login_admin.php)
+
+<h2>Iniciar Sesión Administrador</h2>
+<form method="POST" action="autenticar_admin.php">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="contrasena" placeholder="Contraseña" required>
+    <button type="submit">Entrar</button>
+</form>

9f7bef31c00b7596fb129a080320b6e13f017ad2
iceYami <137309137+iceYami@users.noreply.github.com>
2024-10-20 09:30:05 +0200

    Create videoclub_registro.php

---
+++ videoclub_registro.php
@@ -0,0 +1,9 @@
+// Registro de Nuevos Usuarios (registro.php)
+
+<h2>Registro de Usuario</h2>
+<form method="POST" action="registrar_usuario.php">
+    <input type="text" name="nombre" placeholder="Nombre" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="contrasena" placeholder="Contraseña" required>
+    <button type="submit">Registrarse</button>
+</form>
